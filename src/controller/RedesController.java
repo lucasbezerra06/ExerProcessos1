6c84720c26f6af076cbebd31ad03ee557cf9b5b4
Lucas <lucasbezerra06@gmail.com>
2017-08-19 20:09:26 -0300

    Metodo ip

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -1,8 +1,44 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class RedesController {
 	public RedesController() {
 		super();
+	}
+	
+	public String ip(String SO){
+		StringBuffer ip = new StringBuffer();
+		if(SO.contains("Windows")){
+			
+			try {
+				Process p = Runtime.getRuntime().exec("ipconfig");
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader lerFluxo= new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(lerFluxo);
+				String linha = buffer.readLine();
+				String adaptador="";
+				while(linha != null){
+					if(linha.contains("Adaptador")){
+						adaptador = linha;
+						
+					}
+					if(linha.contains("IPv4")){
+						ip.append(adaptador+"\n"+linha+"\n");
+						
+						
+					}
+					linha = buffer.readLine();
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return ip.toString();
 	}
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,8 +1,13 @@
 package view;
 
+import java.io.IOException;
+
+import controller.RedesController;
+
 public class Main {
 	public static void main(String args[]){
-		
+		RedesController controller = new RedesController();
+		System.out.println(controller.ip(System.getProperty("os.name")));
 	}
 
 }
