f28b1a8797a3bafd3fcdf4e4fd969a220eed5a2a
Lucas <lucasbezerra06@gmail.com>
2017-08-20 21:48:11 -0300

    metodo ping

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -12,33 +12,69 @@ public class RedesController {
 	
 	public String ip(String SO){
 		StringBuffer ip = new StringBuffer();
+		String comando="";
 		if(SO.contains("Windows")){
-			
-			try {
-				Process p = Runtime.getRuntime().exec("ipconfig");
-				InputStream fluxo = p.getInputStream();
-				InputStreamReader lerFluxo= new InputStreamReader(fluxo);
-				BufferedReader buffer = new BufferedReader(lerFluxo);
-				String linha = buffer.readLine();
-				String adaptador="";
-				while(linha != null){
-					if(linha.contains("Adaptador")){
-						adaptador = linha;
-						
-					}
-					if(linha.contains("IPv4")){
-						ip.append(adaptador+"\n"+linha+"\n");
-						
-						
-					}
-					linha = buffer.readLine();
+			comando = "ipconfig";
+		}
+		else if(SO.contains("Linux")){
+			comando = "ifconfig";
+		}
+		try {
+			Process p = Runtime.getRuntime().exec(comando);
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader lerFluxo= new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(lerFluxo);
+			String linha = buffer.readLine();
+			String adaptador="";
+			while(linha != null){
+				if(linha.contains("Adaptador")){
+					adaptador = linha;
+					
 				}
-				
-			} catch (IOException e) {
-				e.printStackTrace();
+				if(linha.contains("IPv4")){
+					ip.append(adaptador+"\n"+linha+"\n");
+					
+					
+				}
+				linha = buffer.readLine();
 			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return ip.toString();
 	}
+	
+	public void ping(String SO){
+		try {
+			Process p = Runtime.getRuntime().exec("ping google.com");
+			InputStream fluxo = p.getInputStream();
+			InputStreamReader lerFluxo= new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(lerFluxo);
+			int tamanho;
+			int ping = 0;
+			int media = 0;
+//			setCharacterEncoding("UTF8");
+			String linha = buffer.readLine();
+			while(linha != null){
+				tamanho = linha.length();
+				
+				if(linha.contains("tempo")){
+					System.out.println(linha.substring(linha.lastIndexOf("=")+1, tamanho));
+					media += Integer.parseInt(linha.substring(linha.lastIndexOf("=")+1, linha.lastIndexOf("ms")));
+					ping++;
+				}
+				/*if(linha.contains("M�dia"))
+					System.out.println(linha.substring(linha.lastIndexOf("M�dia"), tamanho));*/
+				linha = buffer.readLine();
+			}
+			media = media / ping;
+			System.out.println("Média = "+media+"ms");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -6,8 +6,10 @@ import controller.RedesController;
 
 public class Main {
 	public static void main(String args[]){
+		String sistema = System.getProperty("os.name"); 
 		RedesController controller = new RedesController();
-		System.out.println(controller.ip(System.getProperty("os.name")));
+//		System.out.println(controller.ip(sistema));
+		controller.ping(sistema);
 	}
 
 }
